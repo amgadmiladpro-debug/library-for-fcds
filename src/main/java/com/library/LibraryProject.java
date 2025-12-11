d3f2af2e37220deae3c750dbd61635f8731a278a
bebofcds <bebo.boby3@gmail.com>
2025-12-11 22:02:23 +0200

    added some ui

--- src/main/java/com/library/LibraryProject.java
+++ src/main/java/com/library/LibraryProject.java
@@ -7,29 +7,59 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 public class LibraryProject extends Application {
      @Override
     public void start(Stage stage) {
-        Button btn = new Button("Hello JavaFX!");
+        Button btn = new Button("Upload books");
         StackPane root = new StackPane(btn);
+        String[] filePath={""};
+        btn.setOnAction(e -> {
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("Open File");
 
+            // Optional: set extension filters
+            fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Text Files", "*.txt"),
+                new FileChooser.ExtensionFilter("All Files", "*.*")
+            );
+             File selectedFile = fileChooser.showOpenDialog(stage);
+            if (selectedFile != null) {
+                System.out.println("File selected: " + selectedFile.getAbsolutePath());
+                filePath[0]=selectedFile.getAbsolutePath();
+                root.getChildren().remove(btn);
+                // You can now read the file or store its path
+            } else {
+                System.out.println("File selection cancelled.");
+            }
+        });
+        Button displayAllBooks=new Button("Display all books");
+        Button addBooksButton=new Button("Add a new book");
+        Button removeBooksButton=new Button("Remove a book");
+        Button countTotalBooks=new Button("Click to count the books");
+        TextField searchForBook=new TextField("Search for a book");
+        File file=new File(filePath[0]);
+     String []books=readTextFile(file);
         stage.setScene(new Scene(root, 400, 300));
         stage.setTitle("My GUI");
         stage.show();
     }
 
     public static void main(String[] args) {
-    File file =new File("");
-     String []books=readTextFile(file);
-    
+    launch(args);
     }
 
-    private void addToBooks(String[] books, String book) {
-        
-
+    private String[] addToBooks(String[] books, String book) {
+    String[] newBooks=new String[books.length+1];
+    for (int i=0;i<books.length;i++){
+        newBooks[i]=books[i];
+    }        
+    newBooks[books.length]=book;
+    return newBooks;
     }
     private static String[] readTextFile(File file) {
         int i=0;
